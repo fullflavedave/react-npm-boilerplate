c10c30085f1eef8cd5420677eb5f7315c8db632d
fullflavedave <github@fullflavedave.com>
2016-11-04 11:47:50 -0700

    Added typings for promises

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/es2015-promise/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
